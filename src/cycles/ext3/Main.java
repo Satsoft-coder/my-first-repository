b5b6486213d5f41407c36395c8b6854a6e6a4b58
Konstantin Dushin <satsoft@mail.ru>
2024-08-01 16:49:01 +0300

    Исправлено неверное решение cycles.ext3

--- src/cycles/ext3/Main.java
+++ src/cycles/ext3/Main.java
@@ -1,31 +1,45 @@
 package cycles.ext3;
 
+import java.util.Scanner;
+
 public class Main {
     //Все цифры из числа введенного через аргумент к исполняемой
     //программе перемножить между собой и вывести ход вычислений в консоль
 
     public static void main(String[] args) {
-        if (args.length < 1) {
-            System.out.println("Использование: введите несколько чисел");
-            return;
-        }
-        for (String arg : args) {
-            double num1 = Double.parseDouble(arg);
-            for (String s : args) {
-                double num2 = Double.parseDouble(s);
-                System.out.println(num1 + " * " + num2 + " = " + num1 * num2);
-            }
-            System.out.println("");
-        }
 
-/*        double rezult=Double.parseDouble(args[0])
-        for (int i = 1; i < args.length; i++) {
-            double num1 = Double.parseDouble(args[i]);
-            System.out.println(num1);
-            rezult=
-            if (i != args.length-1) {
-                System.out.println("*");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число");
+        int chislo = scan.nextInt();
+        int numZ = 0;
+        int numX = chislo;
+        int rezult = 1;
+
+        while (numX != 0) {    // Разделил на числа цифру
+            numZ = numX % 10;
+            numX /= 10;
+            rezult = rezult * numZ;
+            System.out.println(numZ);
+            if (numX!=0) {
+                System.out.println("x");
             }
-        } */
+        }
+        System.out.println("="+rezult);
     }
 }
+        //неправильное решение
+//        if (args.length < 1) {
+//            System.out.println("Использование: введите несколько чисел");
+//            return;
+//        }
+//        for (String arg : args) {
+//            double num1 = Double.parseDouble(arg);
+//            for (String s : args) {
+//                double num2 = Double.parseDouble(s);
+//                System.out.println(num1 + " * " + num2 + " = " + num1 * num2);
+//            }
+//            System.out.println("");
+//        }
+//
+//    }
+
