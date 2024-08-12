fed1f54846d22b188fc270c3588d2fa7716126c0
Konstantin Dushin <satsoft@mail.ru>
2024-08-12 11:32:17 +0300

    All task completed

--- src/ArrayListAnalog/ArrayListAnalog.java
+++ src/ArrayListAnalog/ArrayListAnalog.java
@@ -3,7 +3,7 @@ package ArrayListAnalog;
 import java.util.ArrayList;
 
 public class ArrayListAnalog {
-    private static Object[]data;
+    private Object[]data;
     private int size=0;
 
     public ArrayListAnalog(int capacity){
@@ -12,21 +12,78 @@ public class ArrayListAnalog {
 
     public void AddElement(Object element) {
         if (size == data.length) {
-            increase();
-            data[size + 1] = element;
-        }
-        else {
-            data[size]=element;
-        }
+            increase(3);
+            }
+        data[size]=element;
         size++;
     }
 
-    private void increase(){
+    public void AddArray(Object [] elementArray) {
+        if (elementArray.length+size> data.length){
+            increase(elementArray.length);
+        }
+        for (Object o : elementArray) {
+            data[size] = o;
+            size++;
+        }
+    }
+
+    //добавление по индексу
+    public void AddElementOnIndex(Object element,int index){
+        if (size == data.length) {
+            increase(3);
+        }
+        Object [] nextData=new Object[data.length];
+            for (int i = 0; i < (size-index); i++) {
+                nextData[i]=data[i+index];
+            }
+            data[index]=element;
+            for (int i = 0; i < size-index; i++) {
+                data[index+i+1]=nextData[i];
+            }
+            size++;
+    }
+
+    //удаление по индексу
+    public void RemoveElementOnIndex(int index){
+        if (index<=size) {
+            Object[] nextData = new Object[data.length];
+            for (int i = 0; i < (size - index - 1); i++) {
+                nextData[i] = data[i + index + 1];
+            }
+            for (int i = 0; i < size - index; i++) {
+                data[index + i] = nextData[i];
+            }
+            data[size]=null;
+            size--;
+        }
+        else {
+            System.out.println("В массиве нет элемента с таким индексом");
+        }
+    }
+
+    private void increase(int addedCapacity){
         int oldDataLength= data.length;
-        Object [] oldData=new Object[oldDataLength+10];
-        System.arraycopy(data,0,oldData,0,oldDataLength);
-        data=new Object[oldDataLength+10];
-        System.arraycopy(oldData,0,data,0,oldDataLength+10);
+        int newDataLength=addedCapacity+oldDataLength;
+
+        Object [] oldData=new Object[newDataLength];
+        //System.arraycopy(data,0,oldData,0,oldDataLength);
+        for (int i = 0; i < oldDataLength; i++) {
+            oldData[i]=data[i];
+        }
+        data=new Object[newDataLength];
+        //System.arraycopy(oldData,0,data,0,newDataLength);
+        for (int i = 0; i < newDataLength; i++) {
+            data[i]=oldData[i];
+        }
+    }
+
+    //удаление всех элементов
+    public void RemoveAll(){
+        for (int i = 0; i < size; i++) {
+            data[i]=null;
+        }
+        size=0;  //по сути можно только size=0 оставить, но что бы освободить память чистим и массив ?! Capacity остается.
     }
 
     public int Size(){return size;}
--- src/ArrayListAnalog/Ex3.java
+++ src/ArrayListAnalog/Ex3.java
@@ -2,10 +2,30 @@ package ArrayListAnalog;
 
 public class Ex3 {
     public static void main(String[] args) {
+
+        //добавление одного элемента
 ArrayListAnalog list =new ArrayListAnalog(3);
         list.AddElement("10");
         list.AddElement("20");
         list.AddElement("30");
         list.AddElement("40");
+        System.out.println(list.Size());
+
+        //добавление массива элементов
+        Object []arrayElement=new Object[]{"90","100","200"};
+        list.AddArray(arrayElement);
+        System.out.println(list.Size());
+
+        //добавление по индексу
+        list.AddElementOnIndex("10",3);
+        System.out.println(list.Size());
+
+        //удаление по индексу
+        list.RemoveElementOnIndex(5);
+        System.out.println(list.Size());
+        list.RemoveElementOnIndex(10);
+
+        //удаление всех элементов
+
     }
 }
