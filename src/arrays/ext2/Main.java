d5927d6ec8f6d6f2708933856e9adb1a84906d18
Konstantin Dushin <satsoft@mail.ru>
2024-08-12 20:49:09 +0300

    Исправлено задание Ext2

--- src/arrays/ext2/Main.java
+++ src/arrays/ext2/Main.java
@@ -2,7 +2,15 @@ package arrays.ext2;
 
 public class Main {
     public static void main(String[] args) {
-        int[] quantity=new int[] {2,5,8,17,45,0,17,42,19,22,0,17,14,25,12};
+        Integer [] quantity=new Integer[10];
+        quantity[0]=5;
+        quantity[1]=0;
+        quantity[2]=10;
+        quantity[3]=74;
+        quantity[4]=12;
+        quantity[5]=34;
+        quantity[8]=44;
+        quantity[9]=4;
 
         //вывод каждого второго элемента массива
         for (int i = 0; i < quantity.length; i++) {
@@ -13,13 +21,13 @@ public class Main {
         }
         System.out.println("---------------");
 
-        //вывод каждого второго элемента массива до 0-го значения
+        //вывод каждого второго элемента массива до null значения
         for (int i = 0; i < quantity.length; i++) {
             if (i%2!=0) {
                 continue;
             }
             else {
-                if (quantity[i]==0){
+                if (quantity[i]==null){
                     break;
                 }
             }
