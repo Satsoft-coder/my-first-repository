d07d05e082192f7704297fa048acc6786de7b0b1
Konstantin Dushin <satsoft@mail.ru>
2024-09-10 14:03:36 +0300

    task1 completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,57 @@
 package org.example;
 
+import java.security.spec.RSAOtherPrimeInfo;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Изучить конструкцию try - catch - finally. Попробовать разделить число на 0 и обработать эту ситуацию
+        //при помощи:
+
+        //try - catch
+        Task1(3,0);
+
+        //try - finally
+        Task2(4,4);
+
+        //try - catch - finally
+        Task3(34,0);
     }
+
+    private static void Task1(int a,int b) {
+        System.out.println("Task1!");
+        try{
+            double c=a/b;
+            System.out.println(a+"/"+b+"="+c);
+        } catch (ArithmeticException e) {
+            //throw new RuntimeException(e);
+            System.out.println("Ошибка деления: "+e+"\n");
+        }
+    }
+
+    private static void Task2(int a,int b){
+        System.out.println("Task2!");
+        try {
+            double c= a/b;
+            System.out.println(a+"/"+b+"="+c);
+        } finally {
+            System.out.println("End Task2\n");
+        }
+
+    }
+    private static void Task3(int a,int b){
+        System.out.println("Task3!");
+        try{
+            double c= a /b;
+            System.out.println(a+"/"+b+"="+c);
+        } catch (ArithmeticException e) {
+            //throw new RuntimeException(e);
+            System.out.println("Ошибка деления: "+e);
+        } finally {
+            System.out.println("End Task3");
+        }
+
+    }
+
 }
